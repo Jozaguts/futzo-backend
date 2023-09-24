adc98822dd07c17d6caa3dda0377604e5765b376
Jose Terrazas <jozaguts@gmail.com>
2023-09-24 16:48:03 -0600

    feat(auth) config added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/auth/{provider}/redirect', function () {
+    $url  = Socialite::driver('facebook')->stateless()->redirect()->getTargetUrl();
+
+    return response()->json(['url' => $url]);
+});
+
+Route::get('/auth/{provider}/callback', function ($provider) {
+
+    $user = Socialite::driver($provider)->stateless()->user();
+    $authUser = User::updateOrCreate(
+        [
+            'email' => $user->getEmail(),
+        ],
+        [
+            'name' => $user->getName(),
+            "{$provider}_id" => $user->getId()
+        ]);
+
+        $token = $authUser->createToken('auth_token');
+
+    return response()->json(['success' => (bool)$token, 'token'=> $token->plainTextToken]);
+});
+
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    return $request->user()->tokens()->delete();
 });

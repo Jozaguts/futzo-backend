7a0e2963e1eed2b4cdad077f974820de4fe80668
Jose Terrazas <jozaguts@gmail.com>
2023-10-24 17:26:28 -0600

    feat(authController): fn me() and resource added

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace app\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AuthenticateController extends Controller
@@ -12,15 +14,18 @@ class AuthenticateController extends Controller
     public function register(StoreUserRequest $request)
     {
         $validated = $request->validated();
-        if(!$validated){
-            return response()->json(['success' => false, 'message' => 'Invalid credentials'], 401);
+
+        try {
+            DB::beginTransaction();
+            $user = User::create($validated);
+            $user->assignRole('admin');
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+            DB::commit();
+        }catch(\Exception $e){
+            DB::rollBack();
+            return response()->json(['success' => false, 'message' => $e->getMessage()], 401);
         }
-        $user = new User();
-        $user->email = $request->email;
-        $plainPassword = $request->password;
-        $user->password = bcrypt($plainPassword);
-        $user->save();
-        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json(['success' => true, 'token' => $token]);
     }
@@ -39,5 +44,16 @@ class AuthenticateController extends Controller
         }
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json(['success' => true, 'token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['success' => true]);
+    }
+
+    public function me(Request $request)
+    {
+        return new UserResource(User::find($request->user()->id));
     }
 }

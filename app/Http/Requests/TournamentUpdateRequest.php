cf84c35d00460fbeaf10a1da2864da3b53ff91e6
Jose Terrazas <jozaguts@gmail.com>
2024-07-08 10:43:58 -0600

    feat(tournament): request added

--- app/Http/Requests/TournamentUpdateRequest.php
+++ app/Http/Requests/TournamentUpdateRequest.php
@@ -5,34 +5,21 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class TournamentStoreRequest extends FormRequest
+class TournamentUpdateRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return[
-            'name' => [
-                'required',
-                'string',
-                'max:255',
-                Rule::unique('tournaments')->where(function ($query) {
-                    return $query->where('category_id', $this->category_id);
-                }),],
+            'name' => 'string|max:255',
             'tournament_format_id' => 'exists:tournament_formats,id',
             'start_date' =>'string|nullable',
             'end_date' => 'string|nullable',
@@ -41,11 +28,11 @@ class TournamentStoreRequest extends FormRequest
             'description' => 'string|nullable',
             'category_id' => 'exists:categories,id|nullable',
             'status' => 'string|nullable',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'location' => 'required|json',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'location' => 'json',
         ];
     }
-    public function messages()
+    public function messages(): array
     {
         return [
             'name.unique' => 'El nombre del torneo ya ha sido tomado.',

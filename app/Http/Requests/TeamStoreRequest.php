bf72851144ecafa57c12377e68ce5658e86752bd
Jose Terrazas <jozaguts@gmail.com>
2024-08-31 21:24:29 -0600

    refactor(request) TeamStoreRequest

--- app/Http/Requests/TeamStoreRequest.php
+++ app/Http/Requests/TeamStoreRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @property mixed category_id
@@ -29,35 +29,44 @@ class TeamStoreRequest extends FormRequest
     public function rules()
     {
 
-        return [
-            'name' => [
-                'required',
-                Rule::unique('teams')->where(function($q) {
-                    return $q->where('category_id', $this->category_id);
-                }),
-            ],
-            'tournament_id' => 'required|exists:tournaments,id',
-            'category_id' => 'required|exists:categories,id',
-            'president_name' => 'string|required',
-            'coach_name' => 'string|required',
-            'phone' => 'string|required',
-            'email' => 'string|email|nullable',
-            'address' => 'string|nullable',
-            'image' => [
-                ...$this->isPrecognitive() ? [] : ['nullable'],
-                'image',
-                'mimes:jpg,png',
-            ],
-            'colors' => [
-                'home' => [
-                  'jersey' => 'string|nullable',
-                  'short' => 'string|nullable'
-              ],
-                'away' => [
-                    'jersey' => 'string|nullable',
-                    'short' => 'string|nullable'
-                ]
-            ],
-        ];
+        return
+            [
+                'team.name' => 'required',
+                'team.address' => 'nullable|string',
+                'team.image' => 'nullable|image|mimes:jpg,png,svg',
+                'team.colors.home.jersey' => 'nullable|string',
+                'team.colors.home.short' => 'nullable|string',
+                'team.colors.away.jersey' => 'nullable|string',
+                'team.colors.away.short' => 'nullable|string',
+                'team.email' => 'nullable|email',
+                'team.phone' => 'nullable|string',
+
+                'president.name' => 'required|string',
+                'president.phone' => 'required|string',
+                'president.email' => 'required|email|string',
+                'president.image' => 'nullable|image|mimes:jpg,png,svg',
+
+                'coach.name' => 'required|string',
+                'coach.phone' => 'required|string',
+                'coach.email' => 'required|email|string',
+                'coach.image' => 'nullable|image|mimes:jpg,png,svg',
+            ];
+    }
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator) {
+            if ($this->teamNameCategoryExists()) {
+                $validator->errors()->add('team.name', 'El equipo ya existe en esta categoría');
+            }
+        });
+
+    }
+    protected function teamNameCategoryExists(): bool
+    {
+        return DB::table('category_team')
+            ->join('teams', 'category_team.team_id', '=', 'teams.id')
+            ->where('teams.name', $this->input('team.name'))
+            ->where('category_team.category_id', $this->input('team.category_id'))
+            ->exists();
     }
 }

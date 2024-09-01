7667e5aa5342a5e5f57b75383430f8ece3167d7b
Jose Terrazas <jozaguts@gmail.com>
2024-08-31 21:24:13 -0600

    refactor(resources) Team and tournament

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -14,6 +14,13 @@ class TeamResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'colors' => $this->colors,
+            'president' => $this->president()->select('id','name','email','phone')->first(),
+            'coach' => $this->coach()->select('id','name','email','phone')->first(),
+            'league' => $this->leagues()->where('league_id', auth()->user()->league_id)->select('leagues.id','leagues.name')->first(),
+        ];
     }
 }

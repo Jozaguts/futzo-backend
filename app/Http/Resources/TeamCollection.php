04c31f784930b977967933e57a7fa1bb2b92708d
Jose Terrazas <jozaguts@gmail.com>
2024-09-03 09:30:54 -0600

    fix(TeamCollection): categories and tournaments removed

--- app/Http/Resources/TeamCollection.php
+++ app/Http/Resources/TeamCollection.php
@@ -2,31 +2,19 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Category;
-use App\Models\Tournament;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class TeamCollection extends ResourceCollection
 {
-    protected mixed $categories;
-    protected mixed $tournaments;
-
-    public function __construct($resource)
-    {
-        parent::__construct($resource);
-
-        $categories = Category::select('id','name')->get();
-        $tournaments = Tournament::select('id','name')->get();
-        $this->categories = $categories;
-        $this->tournaments = $tournaments;
-    }
     public function toArray(Request $request): array
     {
+        $this->collection->transform(function ($team) {
+            $team->slug = str($team->name)->slug('-');
+            return $team;
+        });
         return [
-            'teams' => $this->collection,
-            'categories' => $this->categories,
-            'tournaments' => $this->tournaments,
+            'teams' => $this->collection
         ];
     }
 }

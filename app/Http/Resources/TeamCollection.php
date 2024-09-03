dc11552f4228c664f2ce03de34a19343bb238db1
Jose Terrazas <jozaguts@gmail.com>
2024-09-03 11:35:25 -0600

    fix(teams): collection

--- app/Http/Resources/TeamCollection.php
+++ app/Http/Resources/TeamCollection.php
@@ -7,14 +7,36 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class TeamCollection extends ResourceCollection
 {
+    protected mixed $categories;
+    protected mixed $tournaments;
+
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+
+
+    }
     public function toArray(Request $request): array
     {
-        $this->collection->transform(function ($team) {
-            $team->slug = str($team->name)->slug('-');
-            return $team;
-        });
         return [
-            'teams' => $this->collection
+            'teams' => $this->collection->map(function ($team) {
+                return [
+                    'id' => $team->id,
+                    'name' => $team->name,
+                    'address' => $team->address,
+                    'slug'=> str($team->name)->slug('-'),
+                    'email' => $team->email,
+                    'phone' => $team->phone,
+                    'description' => $team->description,
+                    'image' => $team->image,
+                    'president' => $team->president,
+                    'coach' => $team->coach,
+                    'category' => $team->categories()->where('team_id', $team->id)->first(),
+                    'tournament' => $team->tournaments()->where('team_id', $team->id)->first(),
+                    'colors' => $team->colors,
+                ];
+            }),
+
         ];
     }
 }

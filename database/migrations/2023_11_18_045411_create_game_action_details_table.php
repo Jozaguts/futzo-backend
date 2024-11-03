a403c9058c8ab362d2f55b84e6b298841ab0930a
Jose Terrazas <jozaguts@gmail.com>
2024-11-03 16:14:15 -0600

    fix(migrations) typo

--- database/migrations/2023_11_18_045411_create_game_action_details_table.php
+++ database/migrations/2023_11_18_045411_create_game_action_details_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('penalty_goalkeepers', function (Blueprint $table) {
+        Schema::create('game_action_details', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('action_id');
+            $table->foreign('action_id')->references('id')->on('actions')->onDelete('cascade');
             $table->unsignedBigInteger('game_id');
             $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
-            $table->unsignedBigInteger('team_id');
-            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
             $table->unsignedBigInteger('player_id');
             $table->foreign('player_id')->references('id')->on('players')->onDelete('cascade');
+            $table->time('minute');
+            $table->text('comments')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('goalkeepers');
+        Schema::dropIfExists('game_action_details');
     }
 };

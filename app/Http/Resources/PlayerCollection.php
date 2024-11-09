fdb1b21a0d84d266a35e5b5ecb223addf3bc17fe
Jose Terrazas <jozaguts@gmail.com>
2024-11-09 16:17:28 -0600

    feat(resource): player

--- app/Http/Resources/PlayerCollection.php
+++ app/Http/Resources/PlayerCollection.php
@@ -14,6 +14,31 @@ class PlayerCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return $this->collection->map(function ($player) {
+            return [
+                'id' => $player->id,
+                'name' => $player->user->name,
+                'last_name' => $player->user->last_name,
+                'birthdate' => $player->birthdate,
+                'nationality' => $player->nationality,
+                'team' => [
+                    'id' => $player->team->id,
+                    'name' => $player->team->name,
+                ],
+                'category' => [
+                    'id' => $player->category->id,
+                    'name' => $player->category->name,
+                ],
+                'rol' => 'Jugador', // todo
+                'position' => [
+                    'id' => $player->position->id,
+                    'name' => $player->position->name,
+                    'abbr' => $player->position->abbr,
+                ],
+                'number' => $player->number,
+                'height' => $player->height,
+                'weight' => $player->weight,
+            ];
+        })->toArray();
     }
 }

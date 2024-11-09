5b2a3c853f44a98525f9a29af97a6e913d956abe
Jose Terrazas <jozaguts@gmail.com>
2024-11-09 14:55:06 -0600

    feat(relationships): game                      teams                      players

--- app/Models/GoalDetail.php
+++ app/Models/GoalDetail.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GoalDetail extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable =['game_id', 'team_id', 'player_id', 'goal_time', 'goal_type', 'play_stage', 'goal_schedule', 'goal_half'];
+
+    protected $fillable = ['game_id', 'team_id', 'player_id', 'goal_time', 'goal_type', 'play_stage', 'goal_schedule', 'goal_half'];
+
+    public function game(): belongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function teams(): hasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function players(): hasMany
+    {
+        return $this->hasMany(Player::class);
+    }
 }

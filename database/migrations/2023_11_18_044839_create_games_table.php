b3e1680af9262b7866b137fb42505311c76f4422
Jose Terrazas <jozaguts@gmail.com>
2024-11-16 22:52:05 -0600

    fix(type)

--- database/migrations/2023_11_18_044839_create_games_table.php
+++ database/migrations/2023_11_18_044839_create_games_table.php
@@ -4,39 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('games', function (Blueprint $table) {
-            $table->id();
-            $table->date('date');
-            $table->string('location');
-            $table->unsignedBigInteger('home_team_id');
-            $table->foreign('home_team_id')->references('id')->on('teams')->onDelete('cascade');
-            $table->unsignedBigInteger('away_team_id');
-            $table->foreign('away_team_id')->references('id')->on('teams')->onDelete('cascade');
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-            $table->unsignedBigInteger('winner_team_id')->nullable();
-            $table->foreign('winner_team_id')->references('id')->on('teams');
-            $table->unsignedBigInteger('tournament_id');
-            $table->foreign('tournament_id')->references('id')->on('tournaments')->onDelete('cascade');
-            $table->unsignedBigInteger('league_id');
-            $table->foreign('league_id')->references('id')->on('leagues')->onDelete('cascade');
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('games', function (Blueprint $table) {
+			$table->id();
+			$table->dateTime('date');
+			$table->string('location');
+			$table->unsignedBigInteger('home_team_id');
+			$table->foreign('home_team_id')->references('id')->on('teams')->onDelete('cascade');
+			$table->unsignedBigInteger('away_team_id');
+			$table->foreign('away_team_id')->references('id')->on('teams')->onDelete('cascade');
+			$table->unsignedBigInteger('category_id');
+			$table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+			$table->unsignedBigInteger('winner_team_id')->nullable();
+			$table->foreign('winner_team_id')->references('id')->on('teams');
+			$table->unsignedBigInteger('tournament_id');
+			$table->foreign('tournament_id')->references('id')->on('tournaments')->onDelete('cascade');
+			$table->unsignedBigInteger('league_id');
+			$table->foreign('league_id')->references('id')->on('leagues')->onDelete('cascade');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('games');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('games');
+	}
 };

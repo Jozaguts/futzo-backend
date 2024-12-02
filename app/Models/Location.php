5fbb1514b187c8e1c03a96d15972e8601b0b508f
Jose Terrazas <jozaguts@gmail.com>
2024-12-02 00:49:05 -0600

    feat(Location): tagable

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,16 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Tags\HasTags;
 
 class Location extends Model
 {
-	use HasFactory, SoftDeletes;
+	use HasFactory, SoftDeletes, HasTags;
 
 	protected $fillable = ['name', 'city', 'address', 'autocomplete_prediction'];
 
 	protected $casts = [
 		'autocomplete_prediction' => 'json'
 	];
+	protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 
 	public function tournaments(): BelongsToMany
 	{

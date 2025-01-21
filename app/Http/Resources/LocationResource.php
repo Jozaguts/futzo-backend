c142b71dfc28d40aa5b734fd8f91fcf11a8b1a61
Jose Terrazas <jozaguts@gmail.com>
2025-01-21 00:27:29 -0600

    feat(location): resource added

---
+++ app/Http/Resources/LocationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Location;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Location */
+class LocationResource extends JsonResource
+{
+    private array $imagesAvailable = ['fans', 'game-day', 'goal', 'junior-soccer'];
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => $this->address,
+            'tags' => $this->tags->pluck('name'),
+            'image' => $this->imagesAvailable[array_rand($this->imagesAvailable)],
+            'autocomplete_prediction' => $this->autocomplete_prediction
+        ];
+    }
+}

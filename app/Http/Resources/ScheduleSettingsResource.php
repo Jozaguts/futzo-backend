977006a4cf282f4b1c09da49d4555d5304d76a54
Jose Terrazas <jozaguts@gmail.com>
2025-03-09 15:23:49 -0600

    feat(schedules): calculateTeamsToNextRound added

--- app/Http/Resources/ScheduleSettingsResource.php
+++ app/Http/Resources/ScheduleSettingsResource.php
@@ -19,11 +19,38 @@ class ScheduleSettingsResource extends JsonResource
             'max_teams' => $this->configuration->max_teams ?? null,
             'time_between_games' => $this->configuration->time_between_games ?? null,
             'teams' => $this->teams->count(),
+            'teams_to_next_round' => $this->calculateTeamsToNextRound(),
             'format' => $this->format ?? null,
             'footballType' => $this->footballType ?? null,
             'locations' => $this->locations ?? [],
             'tiebreakers' => $this->configuration->tiebreakers ?? null,
             'phases' => $this->phases ?? null,
         ];
+    }
+
+    private function calculateTeamsToNextRound(): int
+    {
+        $teamsToNextRound = 0;
+        if ($this->format->name === 'Torneo de Liga') {
+            return 1;
+        }
+        $activePhase = collect($this->phases->where('is_active', true)->all());
+        $roundOf16activated = $activePhase->where('name', 'Octavos de Final')->isNotEmpty();
+        if ($roundOf16activated) {
+            return 16;
+        }
+        $quarterFinalsActivated = $activePhase->where('name', 'Cuartos de Final')->isNotEmpty();
+        if ($quarterFinalsActivated) {
+            return 8;
+        }
+        $semiFinalsActivated = $activePhase->where('name', 'Semifinales')->isNotEmpty();
+        if ($semiFinalsActivated) {
+            return 4;
+        }
+        $finalActivated = $activePhase->where('name', 'Final')->isNotEmpty();
+        if ($finalActivated) {
+            return 2;
+        }
+        return $teamsToNextRound;
     }
 }

d060fae06fad3166a211871fb956c9df9ccdbd56
Jose Terrazas <jozaguts@gmail.com>
2025-04-13 22:23:59 -0600

    fix(verification_code): duplicated

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,49 +9,49 @@ use Random\RandomException;
 
 class UserObserver
 {
-	/**
-	 * Handle the User "created" event.
-	 * @throws RandomException
-	 */
-	public function created(User $user): void
-	{
-		if (!is_null($user->phone) && is_null($user->email)) {
-			$phoneNumber = $user->phone;
-			$otp = random_int(1000, 9999);
+    /**
+     * Handle the User "created" event.
+     * @throws RandomException
+     */
+    public function created(User $user): void
+    {
+        if (!is_null($user->phone) && is_null($user->email)) {
+            $phoneNumber = $user->phone;
+            $otp = $user->verification_token;
 
-			Notification::route('whatsapp', $phoneNumber)->notify(new SendOTPNotification($otp, '2222'));
-		}
-	}
+            Notification::route('whatsapp', $phoneNumber)->notify(new SendOTPNotification($otp, $otp));
+        }
+    }
 
-	/**
-	 * Handle the User "updated" event.
-	 */
-	public function updated(User $user): void
-	{
-		//
-	}
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
 
-	/**
-	 * Handle the User "deleted" event.
-	 */
-	public function deleted(User $user): void
-	{
-		//
-	}
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
 
-	/**
-	 * Handle the User "restored" event.
-	 */
-	public function restored(User $user): void
-	{
-		//
-	}
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
 
-	/**
-	 * Handle the User "force deleted" event.
-	 */
-	public function forceDeleted(User $user): void
-	{
-		//
-	}
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
 }

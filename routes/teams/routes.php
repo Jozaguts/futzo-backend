eaef26d4e0c1475fad5bf4d2f46e178c29a686b6
Jose Terrazas <jozaguts@gmail.com>
2025-04-26 19:53:35 -0600

    feat(routes): add endpoint to download teams template

--- routes/teams/routes.php
+++ routes/teams/routes.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('teams')->group(function () {
     Route::get('list', [TeamsController::class, 'list']);
+    Route::get('template', [TeamsController::class, 'downloadTeamsTemplate']);
     Route::get('', [TeamsController::class, 'index']);
     Route::get('{id}', [TeamsController::class, 'show']);
     Route::post('', [TeamsController::class, 'store']);
     Route::put('{id}', [TeamsController::class, 'update']);
     Route::delete('{id}', [TeamsController::class, 'destroy']);
-
 });

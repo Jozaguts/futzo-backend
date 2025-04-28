4a6d68ca72e1df7d9a4f855f390f855bda35bae5
Jose Terrazas <jozaguts@gmail.com>
2025-04-28 00:51:02 -0600

    feat(players): add route for downloading players template

--- routes/players/routes.php
+++ routes/players/routes.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\PlayersController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('players')->group(function () {
-	Route::get('', [PlayersController::class, 'index'])->name('players.index');
-	Route::get('{player}', [PlayersController::class, 'show'])->name('players.show');
-	Route::post('', [PlayersController::class, 'store'])->name('players.store');
-	Route::put('{player}', [PlayersController::class, 'update'])->name('players.update');
-	Route::delete('{player}', [PlayersController::class, 'destroy'])->name('players.destroy');
-	Route::post('import', [PlayersController::class, 'import'])->name('players.import');
+    Route::get('template', [PlayersController::class, 'downloadPlayersTemplate'])->name('players.export.template');
+    Route::get('{player}', [PlayersController::class, 'show'])->name('players.show');
+    Route::get('', [PlayersController::class, 'index'])->name('players.index');
+    Route::post('import', [PlayersController::class, 'import'])->name('players.import');
+    Route::put('{player}', [PlayersController::class, 'update'])->name('players.update');
+    Route::post('', [PlayersController::class, 'store'])->name('players.store');
+    Route::delete('{player}', [PlayersController::class, 'destroy'])->name('players.destroy');
 });

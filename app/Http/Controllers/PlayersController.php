23b2b82472cae5b5b910f22acca89d9aeeb3d6f6
Jose Terrazas <jozaguts@gmail.com>
2025-04-29 23:45:38 -0600

    feat(players): enhance import functionality with header validation and error handling

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\RegisteredTeamCoach;
+use App\Events\RegisteredTeamPresident;
 use App\Exports\PlayersTemplateExport;
 use App\Http\Requests\PlayerStoreRequest;
 use App\Http\Requests\PlayerUpdateRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\TeamStoreRequest;
 use App\Http\Resources\PlayerCollection;
 use App\Imports\PlayersImport;
 use App\Models\Player;
+use App\Models\Position;
+use App\Models\Team;
+use App\Models\Tournament;
+use App\Models\User;
+use App\Services\Builders\PlayerBuilder;
 use App\Services\PlayerService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 use PhpOffice\PhpSpreadsheet\Exception;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class PlayersController extends Controller
@@ -61,10 +75,48 @@ class PlayersController extends Controller
 
     }
 
+    /**
+     * @throws \Throwable
+     * @throws FileDoesNotExist
+     * @throws FileIsTooBig
+     */
     public function import(Request $request): \Illuminate\Http\JsonResponse
     {
-        Excel::import(new PlayersImport(), $request->file('file'));
-        return response()->json('File imported successfully');
+        try {
+            $spreadsheet = IOFactory::load($request->file('file'));
+            $team = Team::find($request->get('team_id'));
+            $sheetNames = $spreadsheet->getSheetNames();
+            $found = false;
+            $playersData = [];
+            foreach ($sheetNames as $name) {
+                $sheet = $spreadsheet->getSheetByName($name);
+                if (!$sheet) {
+                    continue;
+                }
+
+                $header = $sheet->rangeToArray('A1:O1', null, true, true, true)[1];
+
+                if ($this->isValidHeader($header)) {
+                    $found = true;
+                    $rows = $sheet->toArray(null, true, true, true);
+                    array_shift($rows); // quitar header
+                    $playersData = $rows;
+                    break;
+                }
+            }
+            if (!$found) {
+                return response()->json([
+                    'message' => 'No se encontró una hoja de datos válida. Asegúrese de que las columnas coincidan con el formato requerido.',
+                ], 422);
+            }
+            foreach ($playersData as $row) {
+                $this->storePlayerFromRow($row, $team);
+            }
+            return response()->json('File imported successfully');
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
     }
 
     /**
@@ -74,5 +126,70 @@ class PlayersController extends Controller
     public function downloadPlayersTemplate(): BinaryFileResponse
     {
         return Excel::download(new PlayersTemplateExport(), 'jugadores_template.xlsx');
+    }
+
+    private function isValidHeader($header): bool
+    {
+        $expected = [
+            'A' => 'nombre',
+            'B' => 'apellido',
+            'C' => 'correo',
+            'D' => 'teléfono',
+            'E' => 'fecha_nacimiento',
+            'F' => 'nacionalidad',
+            'G' => 'posicion',
+            'H' => 'numero',
+            'I' => 'altura',
+            'J' => 'peso',
+            'K' => 'pie_dominante',
+            'L' => 'notas_medicas'
+        ];
+        foreach ($expected as $column => $expectedValue) {
+            if (trim($header[$column]) !== $expectedValue) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @throws FileDoesNotExist
+     * @throws FileIsTooBig
+     * @throws \Throwable
+     */
+    private function storePlayerFromRow($row, Team $team): void
+    {
+        $position_id = Position::whereLike('name', $row['G'])->first()?->id;
+        $data = [
+            'basic' => [
+                'name' => $row['A'],
+                'last_name' => $row['B'],
+                'birthdate' => Carbon::create($row['E'])?->toDateString(),
+                'nationality' => $row['F'],
+                'team_id' => $team?->id,
+                'category_id' => $team->category()->id,
+            ],
+            'details' => [
+                'position_id' => $position_id,
+                'number' => $row['H'],
+                'height' => $row['I'],
+                'weight' => $row['J'],
+                'dominant_foot' => $row['K'],
+                'medical_notes' => $row['L'],
+            ],
+            'contact' => [
+                'email' => $row['C'],
+                'phone' => $row['D'],
+                'notes' => $row['L'],
+            ]
+        ];
+
+        $formRequest = PlayerStoreRequest::create('', 'POST', $data);
+        $formRequest->setContainer(app())->setRedirector(app('redirect'));
+        $formRequest->validateResolved();
+        $builder = new PlayerBuilder;
+        $service = new PlayerService($builder);
+        $service->store($formRequest);
+
     }
 }

7b3b612f1c1978a65ce6e3e7d55faf14f03f3043
Jose Terrazas <jozaguts@gmail.com>
2025-05-20 17:13:58 -0600

    fix(TeamResource): update league retrieval to handle potential null user

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -14,13 +14,14 @@ class TeamResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'colors' => $this->colors,
-            'president' => $this->president()->select('id','name','email','phone')->first(),
-            'coach' => $this->coach()->select('id','name','email','phone')->first(),
-            'league' => $this->leagues()->where('league_id', auth()->user()->league_id)->select('leagues.id','leagues.name')->first(),
+            'president' => $this->president()->select('id', 'name', 'email', 'phone')->first(),
+            'coach' => $this->coach()->select('id', 'name', 'email', 'phone')->first(),
+            'league' => $this->leagues()->where('league_id', auth()?->user()?->league_id)->select('leagues.id', 'leagues.name')->first(),
         ];
     }
 }

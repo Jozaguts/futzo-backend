48ffba445b81c464dfee175f285877dd1b2945a1
Jose Terrazas <jozaguts@gmail.com>
2025-05-24 20:24:22 -0600

    test(user): refactor user tests to use it() syntax

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,71 +6,57 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\InitUser;
 use Tests\TestCase;
 
-class UserTest extends TestCase
-{
-    use RefreshDatabase, InitUser;
+it('get users', function () {
+    $this->json('POST', '/auth/register', [
+        'name' => 'John',
+        'email' => 'test@test.com',
+        'password' => 'password'
+    ]);
 
+    $this->assertDatabaseHas('users', [
+        'name' => 'John',
+        'email' => 'test@test.com',
+        'image' => 'https://ui-avatars.com/api/?name=John&color=9155fd&background=F9FAFB'
+    ]);
+});
 
-    public function test_register_user()
-    {
-        $this->json('POST', '/auth/register', [
-            'name' => 'John',
-            'email' => 'test@test.com',
-            'password' => 'password'
+it('get users with pagination', function () {
+    $user = $this->initUser();
+    $response = $this
+        ->actingAs($user)
+        ->json('GET', '/api/v1/me');
+
+    $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'name',
+            'email',
+            'roles',
+            'league'
         ]);
+});
+it('get user by id', function () {
+    $user = $this->initUser();
 
-        $this->assertDatabaseHas('users', [
+    $response = $this
+        ->actingAs($user)
+        ->json('PUT', "/api/v1/admin/profile/$user->id", [
             'name' => 'John',
-            'email' => 'test@test.com',
-            'image' => 'https://ui-avatars.com/api/?name=John&color=9155fd&background=F9FAFB'
+            'email' => 'test2@gmail.com',
         ]);
-    }
+    $response->assertStatus(200);
+});
 
-    public function test_me_endpoint()
-    {
-        $user = $this->initUser();
+it('get user by id with wrong id', function () {
+    $user = $this->initUser();
+    $user2 = $this->initUser();
 
-
-        $response = $this
-            ->actingAs($user)
-            ->json('GET', '/api/v1/me');
-
-        $response
-            ->assertStatus(200)
-            ->assertJsonStructure([
-                'name',
-                'email',
-                'roles',
-                'league'
-            ]);
-    }
-
-    public function test_edit_user()
-    {
-        $user = $this->initUser();
-
-        $response = $this
-            ->actingAs($user)
-            ->json('PUT', "/api/v1/admin/profile/$user->id", [
-                'name' => 'John',
-                'email' => 'test2@gmail.com',
-            ]);
-        $response->assertStatus(200);
-    }
-
-    public function test_ensure_user_owns_profile()
-    {
-        $user = $this->initUser();
-        $user2 = $this->initUser();
-
-        $response = $this
-            ->actingAs($user2)
-            ->json('PUT', "/api/v1/admin/profile/$user->id", [
-                'name' => 'John',
-                'email' => 'test@gmail.com',
-                'phone' => '+523222397179',
-            ]);
-        $response->assertStatus(403);
-    }
-
-}
+    $response = $this
+        ->actingAs($user2)
+        ->json('PUT', "/api/v1/admin/profile/$user->id", [
+            'name' => 'John',
+            'email' => 'test@gmail.com',
+            'phone' => '+523222397179',
+        ]);
+    $response->assertStatus(403);
+});

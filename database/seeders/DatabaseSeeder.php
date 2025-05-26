61d5be309fd599c9932e52eb644f8404122fb542
Jose Terrazas <jozaguts@gmail.com>
2025-05-26 01:49:37 -0600

    refactor(seeder): streamline database seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\League;
-use App\Models\User;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 use Random\RandomException;
 
 class DatabaseSeeder extends Seeder
@@ -21,34 +19,26 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesTableSeeder::class,
+            CountriesSeeder::class,
+            PositionsTableSeeder::class,
+            CategoriesTableSeeder::class,
+            TournamentFormatTableSeeder::class,
+            FootballTypesTableSeeder::class,
+            DefaultTournamentConfigurationTableSeeder::class,
+            ActionsTableSeeder::class,
+            CouponsTableSeeder::class,
+            LeaguesTableSeeder::class,
+            AdminUserSeeder::class,
+            LocationsTableSeeder::class,
+            FieldsTableSeeder::class,
+            TournamentTableSeeder::class,
+            TeamsTableSeeder::class,
         ]);
-        $user = User::create([
-            'name' => 'Sagit Gutierrez',
-            'email' => 'admin@futzo.io',
-            'verified_at' => now(),
-            'verification_token' => random_int(1000, 9999),
-            'phone' => '3221231231',
-            'password' => '$2y$10$RENqDsgT5rr0sjujwq1v4uoTXC9K9f7KMa1ilMFOdG2DMf7Xwm2TS', //password.
-            'remember_token' => Str::random(10),
-        ]);
-        $user->assignRole('super administrador');
-        $this->call(CountriesSeeder::class);
-        $this->call(PositionsTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(TournamentFormatTableSeeder::class);
-        $this->call(FootballTypesTableSeeder::class);
-        $this->call(DefaultTournamentConfigurationTableSeeder::class);
-        $this->call(ActionsTableSeeder::class);
-        $this->call(CouponsTableSeeder::class);
-        $this->call(LeaguesTableSeeder::class);
-        $this->call(LocationsTableSeeder::class);
-        $this->call(FieldsTableSeeder::class);
 //        $this->call(LeagueFieldTableSeeder::class);
-        $user->league_id = League::first()->id;
-        $user->save();
+
 //        $this->call(UserSeeder::class);
-        $this->call(TournamentTableSeeder::class);
-        $this->call(TeamsTableSeeder::class);
+//        $this->call(TournamentTableSeeder::class);
+//        $this->call(TeamsTableSeeder::class);
 //        $this->call(PlayerSeeder::class);
 //        $this->call(GameSeeder::class);
 //        $this->call(GamePlayerSeeder::class);

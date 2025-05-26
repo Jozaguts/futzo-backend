2a2a8133978973ebd8cead3393dfddddf68ef338
Jose Terrazas <jozaguts@gmail.com>
2025-05-26 01:50:36 -0600

    fix(routes): improve formatting and add logout route name

--- routes/auth/routes.php
+++ routes/auth/routes.php
@@ -8,29 +8,29 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::prefix('auth')->group(function () {
 
-	Route::post('/register', [AuthenticateController::class, 'register']);
-	Route::get('/{provider}/redirect', function ($provider) {
-		$url = Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
+    Route::post('/register', [AuthenticateController::class, 'register']);
+    Route::get('/{provider}/redirect', function ($provider) {
+        $url = Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
 
-		return response()->json(['url' => $url]);
-	});
-	Route::get('/{provider}/callback', function ($provider) {
+        return response()->json(['url' => $url]);
+    });
+    Route::get('/{provider}/callback', function ($provider) {
 
-		$user = Socialite::driver($provider)->stateless()->user();
-		$authUser = User::updateOrCreate(
-			[
-				'email' => $user->getEmail(),
-			],
-			[
-				'name' => $user->getName(),
-				"{$provider}_id" => $user->getId(),
-				'verified_at' => now(),
-			]);
+        $user = Socialite::driver($provider)->stateless()->user();
+        $authUser = User::updateOrCreate(
+            [
+                'email' => $user->getEmail(),
+            ],
+            [
+                'name' => $user->getName(),
+                "{$provider}_id" => $user->getId(),
+                'verified_at' => now(),
+            ]);
 
-		Auth::login($authUser);
+        Auth::login($authUser);
 
-		return response()->noContent();
-	});
-	Route::post('login', [AuthenticateController::class, 'login']);
-	Route::post('logout', [AuthenticateController::class, 'logout']);
+        return response()->noContent();
+    });
+    Route::post('login', [AuthenticateController::class, 'login']);
+    Route::post('logout', [AuthenticateController::class, 'logout'])->name('logout');
 });

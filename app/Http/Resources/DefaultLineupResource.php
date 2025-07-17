d966442b82d171e24ab1bb19278e618c4802991c
Jose Terrazas <jozaguts@gmail.com>
2025-07-16 18:12:52 -0600

    feat(DefaultLineupResource): enhance goalkeeper data structure with player details and default values

--- app/Http/Resources/DefaultLineupResource.php
+++ app/Http/Resources/DefaultLineupResource.php
@@ -13,7 +13,20 @@ class DefaultLineupResource extends JsonResource
     {
         return [
             'formation' => $this->resource->defaultLineup->formation->value,
-            'goalkeeper' => $this->resource->defaultlineup,
+            'goalkeeper' => $this->resource->defaultlineup->players->where('position_id',1)->transform(function ($player) {
+                return [
+                    'abbr' => $player->position->abbr,
+                    'number'=> $player->number,
+                    'name'=> $player->user->name,
+                    'goals'=> 0,
+                    'cards'=> [
+                        'red' => false,
+                        'yellow'=> false,
+                        'doble_yellow_card'=> false
+                    ],
+                    'substituted' => false
+                ];
+            })->toArray(),
             'defenses' => $this->updated_at,
             'midfielders' => $this->updated_at,
             'forwards' => $this->updated_at

9f6af501041e0c42a957402f7bba76342b1b151b
Jose Terrazas <jozaguts@gmail.com>
2025-07-16 22:54:31 -0600

    fix(routes): add endpoint to retrieve players for a specific team

--- routes/teams/routes.php
+++ routes/teams/routes.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('teams')->group(function () {
     Route::post('{team}/players/{player}/assign', [TeamsController::class, 'assignPlayer']);
     Route::get('{team}/formation', [TeamsController::class, 'formation']);
+    Route::get('{team}/players', [TeamsController::class, 'players']);
     Route::get('list', [TeamsController::class, 'list']);
     Route::get('template', [TeamsController::class, 'downloadTeamsTemplate']);
     Route::get('search', [TeamsController::class, 'search']);

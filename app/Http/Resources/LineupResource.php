eab4acf2fe5200bc8142a687e87c0f4151b4df50
Jose Terrazas <jozaguts@gmail.com>
2025-07-21 12:20:19 -0600

    refactor(LineupResource.php): add team resource to lineup response for improved data structure

--- app/Http/Resources/LineupResource.php
+++ app/Http/Resources/LineupResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -43,6 +44,7 @@ class LineupResource extends JsonResource
 
         return [
             'team_id' => $this->resource->team_id,
+            'team' => new TeamResource(Team::find($this->resource->team_id)),
             'name' => $formation->name ?? '',
             'goalkeeper' => $this->fillPlayers(
                 $this->transformPlayers($this->positions['goalkeeper']),

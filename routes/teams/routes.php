ac55b3f97cb7281c3c62f2ea1de44d8a22d3f61f
Jose Terrazas <jozaguts@gmail.com>
2025-07-22 08:44:19 -0600

    refactor(routes.php): add routes for lineup player management

--- routes/teams/routes.php
+++ routes/teams/routes.php
@@ -16,9 +16,11 @@ Route::prefix('teams')->group(function () {
     Route::get('', [TeamsController::class, 'index'])->withoutMiddleware('auth:sanctum');
     Route::get('{id}', [TeamsController::class, 'show'])->withoutMiddleware('auth:sanctum');
     Route::post('{team}/default-lineup-players', [TeamsController::class, 'addDefaultLineupPlayer']);
+    Route::post('{team}/lineup-players', [TeamsController::class, 'addLineupPlayer']);
     Route::post('import', [TeamsController::class, 'import']);
     Route::post('', [TeamsController::class, 'store'])->withoutMiddleware('auth:sanctum');
     Route::put('{team}/default-lineup-players/{defaultLineupPlayer}', [TeamsController::class, 'updateDefaultLineupAvailableTeamPlayers']);
+    Route::put('{team}/lineup-players/{lineupPlayer}', [TeamsController::class, 'updateLineupAvailableTeamPlayers']);
     Route::put('{team}/formation', [TeamsController::class, 'updateFormation']);
     Route::put('{id}', [TeamsController::class, 'update']);
     Route::delete('{id}', [TeamsController::class, 'destroy']);

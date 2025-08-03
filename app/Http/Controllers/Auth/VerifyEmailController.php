b8ea3a8bc560c92eb6250e02cf891a4122cf0b5d
Jose Terrazas <jozaguts@gmail.com>
2025-08-03 01:01:11 -0600

    refactor(verification):  twilio verification code added

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -7,31 +7,52 @@ use App\Http\Requests\VerifyEmailRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Client;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
+     * @throws TwilioException
      */
     public function __invoke(VerifyEmailRequest $request): JsonResponse
     {
-        $data = $request->validated();
+        $phone = $request->input('phone',false);
+        $code = $request->input('code',false);
+        $email = $request->input('email',false);
+        $verified = false;
+        $message = 'Error al verificar, por favor intente mas tarde';
+        $status = 401;
 
-        $user = User::where('verification_token', $data['code'])
-            ->where(function ($query) use ($data) {
-                if (isset($data['phone'])) {
-                    $query->where('phone', $data['phone']);
-                } else {
-                    $query->where('email', $data['email']);
+        if($phone){
+            $twilio = new Client(config('services.twilio.sid'), config('services.twilio.token'));
+            $check = $twilio->verify->v2->services(config('services.twilio.verify_sid'))
+                ->verificationChecks
+                ->create([
+                    'code' => $code,
+                    'to' => $phone,
+                ]);
+            $verified = $check->status === 'approved';
+        } else if($email){
+            $verified = User::where('email', $email)
+                ->where('verification_token', $code)
+                ->exists();
+        }
+        if ($verified) {
+            $user = User::where(static function ($q) use ($phone, $email) {
+                if (!empty($email)) {
+                    $q->where('email', $email);
+                } elseif (!empty($phone)) {
+                    $q->where('phone', $phone);
                 }
-            })->firstOrFail();
-
-        $this->markEmailAsVerified($user);
-
-        event(new Verified($user));
-
-        return response()->json(['message' => 'Cuenta verificada exitosamente.']);
+            })->first();
+            $this->markEmailAsVerified($user);
+            event(new Verified($user));
+            $message = 'Cuenta verificada exitosamente.';
+            $status = 200;
+        }
+        return response()->json(['message' => $message],$status);
     }
 
     private function markEmailAsVerified(User $user): void

48dcd91402f02e81562f5ac08e15cf83590bb2a3
Jose Terrazas <jozaguts@gmail.com>
2025-08-05 15:44:41 -0600

    trait(Lineup): is_headline added

--- app/Traits/LineupTrait.php
+++ app/Traits/LineupTrait.php
@@ -13,27 +13,28 @@ trait LineupTrait
         $lineup->load(['defaultLineup.defaultLineupPlayers','team.players']);
         $defaultLineupPlayers =  $lineup->defaultLineup?->defaultLineupPlayers;
         if (!isEmpty($defaultLineupPlayers)) {
-            foreach ($defaultLineupPlayers as $defaultPLayer) {
+            $defaultLineupPlayers->each(function ($defaultPLayer) use ($lineup) {
                 // todo DON'T REGISTER players IN LINEUP_PLAYERS table if it's in SUSPEND STATUS
                 $lineup->lineupPlayers()->save(
                     LineupPlayer::updateOrCreate([
                         'lineup_id' => $lineup->id,
                         'player_id' => $defaultPLayer->player_id,
                     ], [
-                        'is_headline' => false,
+                        'is_headline' => $defaultPLayer->field_location <= 11,
                         'field_location' => $defaultPLayer->field_location,
                         'substituted' => false,
                     ])
                 );
-            }
+            });
+
         }else{
             $lineup->team?->players?->each(function($player, $key) use($lineup){
                 LineupPlayer::updateOrCreate([
                     'lineup_id' => $lineup->id,
                     'player_id' => $player->id,
                 ], [
-                    'is_headline' => false,
-                    'field_location' => $key + 1, // without field_location
+                    'is_headline' => $key + 1 <= 11,
+                    'field_location' => $key + 1,
                     'substituted' => false,
                 ]);
             });

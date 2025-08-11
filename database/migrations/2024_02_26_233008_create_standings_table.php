553ea2d0c878fb044105c556c67c054855e8a9a8
Jose Terrazas <jozaguts@gmail.com>
2025-08-11 03:47:11 -0600

    refactor(migrations): standings table

--- database/migrations/2024_02_26_233008_create_standings_table.php
+++ database/migrations/2024_02_26_233008_create_standings_table.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Models\League;
+use App\Models\Tournament;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -23,24 +26,42 @@ return new class extends Migration
                 ->references('id')
                 ->on('team_tournament')
                 ->cascadeOnDelete();
+            $table->unsignedBigInteger('updated_after_game_id');
+            $table->foreign('updated_after_game_id')
+                ->references('id')
+                ->on('games')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('tournament_phase_id');
+            $table->foreign('tournament_phase_id')
+                ->references('id')
+                ->on('tournament_phases')
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(Tournament::class)->constrained('tournaments');
+            $table->foreignIdFor(League::class)->constrained('leagues');
+            $table->unique(['team_tournament_id', 'tournament_phase_id'], 'standings_unique_phase');
             // played games
-            $table->integer('pg')->default(0);
+            $table->integer('matches_played')->default(0);
             // won games
-            $table->integer('w')->default(0);
+            $table->integer('wins')->default(0);
             // drawn games
-            $table->integer('d')->default(0);
+            $table->integer('draws')->default(0);
             // lost games
-            $table->integer('l')->default(0);
+            $table->integer('losses')->default(0);
             // goals in favor
-            $table->integer('gf')->default(0);
+            $table->integer('goals_for')->default(0);
             // goals against
-            $table->integer('ga')->default(0);
+            $table->integer('goals_against')->default(0);
             // goal difference
-            $table->integer('gd')->default(0);
+            $table->integer('goal_difference')->default(0);
             // points
-            $table->integer('pts')->default(0);
+            $table->integer('points')->default(0);
+            // En algunas ligas, las tarjetas y sanciones afectan la clasificación en desempates.
+            $table->integer('fair_play_points')->default(0);
             // las five games
             $table->string('last_5')->default('-----');
+            $table->integer('rank')->nullable()->default(null);
+
             $table->softDeletes();
 
             $table->timestamps();

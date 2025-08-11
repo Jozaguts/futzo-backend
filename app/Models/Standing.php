9d0973d7976a0eeebad1d3170506f1d08b756400
Jose Terrazas <jozaguts@gmail.com>
2025-08-11 03:49:06 -0600

    refactor(model): Standings

--- app/Models/Standing.php
+++ app/Models/Standing.php
@@ -7,7 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Standing extends Model
 {
-    use HasFactory;
-    protected $fillable = ['team_id','team_tournament_id','pg','w','d','l','gf','ga','gd', 'pts', 'last_5',
+    protected $fillable = [
+        'team_id',
+        'team_tournament_id',
+        'updated_after_game_id',
+        'tournament_phase_id',
+        'matches_played',
+        'tournament_id',
+        'league_id',
+        'wins',
+        'draws',
+        'losses',
+        'goals_for',
+        'goals_against',
+        'goal_difference',
+        'points',
+        'fair_play_points',
+        'last_5',
+        'rank',
     ];
 }

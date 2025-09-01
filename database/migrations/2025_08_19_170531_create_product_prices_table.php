53ed24f0d44149be303d992cf33165f7a807564f
Jose Terrazas <jozaguts@gmail.com>
2025-09-01 02:43:40 -0600

    fix(dependencies):faker

--- database/migrations/2025_08_19_170531_create_product_prices_table.php
+++ database/migrations/2025_08_19_170531_create_product_prices_table.php
@@ -9,68 +9,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        if (! Schema::hasTable('product_prices')) {
-            Schema::create('product_prices', function (Blueprint $table) {
-                $table->id();
-                $table->foreignIdFor(Product::class)->constrained('products');
-                $table->foreignIdFor(Currency::class)->constrained('currencies');
-                $table->enum('billing_period', ['month', 'year','one_time']);
-                $table->string('plan_slug');
-                $table->bigInteger('price');
-                $table->string('variant');
-                $table->string('stripe_price_id')->nullable();
-                $table->string('stripe_product_id')->nullable();
-                $table->boolean('active')->default(false);
-                $table->timestamps();
-            });
-//            $productPrices = [
-//                [
-//                    'product_id' => 1,
-//                    'currency_id' => 1,
-//                    'price' => 49900,
-//                    'billing_period' => 'month',
-//                ],
-//                [
-//                    'product_id' => 1,
-//                    'currency_id' => 1,
-//                    'price' => 526800,
-//                    'billing_period' => 'annually',
-//                ],
-//                [
-//                    'product_id' => 2,
-//                    'currency_id' => 1,
-//                    'price' => 79900,
-//                    'billing_period' => 'month',
-//                ],
-//                [
-//                    'product_id' => 2,
-//                    'currency_id' => 1,
-//                    'price' => 838800,
-//                    'billing_period' => 'annually',
-//                ],
-//                [
-//                    'product_id' => 3,
-//                    'currency_id' => 1,
-//                    'price' => 149900,
-//                    'billing_period' => 'month',
-//                ],
-//                [
-//                    'product_id' => 3,
-//                    'currency_id' => 1,
-//                    'price' => 1498800,
-//                    'billing_period' => 'annually',
-//                ],
-//            ];
-//            foreach ($productPrices as $productPrice) {
-//                ProductPrice::updateOrCreate([
-//                    'product_id' => $productPrice['product_id'],
-//                    'currency_id' => $productPrice['currency_id'],
-//                    'billing_period' => $productPrice['billing_period'],
-//                ],[
-//                    'price' => $productPrice['price'],
-//                ]);
-//            }
-        }
+        Schema::create('product_prices', static function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Product::class)->constrained('products');
+            $table->foreignIdFor(Currency::class)->constrained('currencies');
+            $table->enum('billing_period', ['month', 'year','one_time']);
+            $table->string('plan_slug');
+            $table->bigInteger('price');
+            $table->string('variant');
+            $table->string('stripe_price_id')->nullable();
+            $table->string('stripe_product_id')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
 
     }
 

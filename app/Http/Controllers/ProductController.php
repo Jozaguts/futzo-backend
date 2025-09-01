24719f237df1ed8d1887a88c1e36c01bc69386fd
Jose Terrazas <jozaguts@gmail.com>
2025-09-01 10:45:17 -0600

    fix(Product controller): check if product prices have benn loaded

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductCollection;
 use App\Models\Product;
+use App\Models\ProductPrice;
 
 class ProductController extends Controller
 {
     public function prices(): ProductCollection
     {
+        if (ProductPrice::count() === 0) {
+            abort(404, 'No product prices found');
+        }
          return new ProductCollection(Product::with('productPrices.currency')->get());
     }
 }

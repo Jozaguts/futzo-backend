0310c09467264a2b087e3041fd6fa60878bf7072
Jose Terrazas <jozaguts@gmail.com>
2025-09-07 14:00:03 -0600

    fix(location): test

--- tests/Feature/LocationTest.php
+++ tests/Feature/LocationTest.php
@@ -5,14 +5,54 @@ namespace Tests\Feature;
 
 it('stores a location correctly', function () {
     $response = $this->postJson('/api/v1/admin/locations', [
-        'name' => 'Location 1',
-        'address' => 'Address 1',
-        'place_id' => 'test_place_id',
-        'position' => [
-            'lat' => -34.397,
-            'lng' => 150.644,
+        "name" => "Tesistán",
+        "address" => "45200 Tesistán, Jal., México",
+        "place_id"=> "ChIJWz3KifemKIQRtDfNWFAlOpI",
+        "position"=> [
+            "lat"=> 20.8016245,
+            "lng"=> -103.47919460000001
         ],
+        "tags"=> ['test tag'],
+        "fields"=> [
+        [
+            "id"=> 1,
+            "name"=> "campo 1",
+            "windows"=> [
+            "mon"=> [],
+                "tue"=> [],
+                "wed"=> [],
+                "thu"=> [],
+                "fri"=> [
+                    [
+                        "start"=> "09:00",
+                        "end"=> "17:00"
+                    ]
+                ],
+                "sat"=> [
+                    [
+                        "start"=> "09:00",
+                        "end"=> "17:00"
+                    ]
+                ],
+                "sun"=> [
+                    [
+                        "start"=> "09:00",
+                        "end"=> "17:00"
+                    ]
+                ],
+                "all"=> []
+            ]
+        ]
+    ],
+        "fields_count"=> 1,
+        "steps"=> [
+        "location"=> [
+            "completed"=> true
+        ],
+        "fields"=> [
+            "completed"=> true
+        ]
+    ]
     ]);
-
     $response->assertCreated();
 });

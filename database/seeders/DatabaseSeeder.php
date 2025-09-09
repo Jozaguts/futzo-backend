edeb95b43338e37fd587c463b32901cff536481a
Jose Terrazas <jozaguts@gmail.com>
2025-09-09 16:37:12 -0600

    refactor(seeder): prod seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 use Random\RandomException;
 
 class DatabaseSeeder extends Seeder
@@ -45,5 +45,6 @@ class DatabaseSeeder extends Seeder
             ];
         }
         $this->call($seeders);
+        Artisan::call('sync:stripe-prices');
     }
 }

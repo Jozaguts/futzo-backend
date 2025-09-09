c4d0cd8194c87dd177c362a13102e20b67521f15
Jose Terrazas <jozaguts@gmail.com>
2025-09-09 16:37:02 -0600

    refactor(location): remove windows key

--- app/Http/Resources/LocationCollection.php
+++ app/Http/Resources/LocationCollection.php
@@ -9,48 +9,46 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class LocationCollection extends ResourceCollection
 {
     private array $imagesAvailable = ['fans', 'game-day', 'goal', 'junior-soccer'];
-    private const DOW_LABELS = [
-        0 => 'domingo',
-        1 => 'lunes',
-        2 => 'martes',
-        3 => 'miércoles',
-        4 => 'jueves',
-        5 => 'viernes',
-        6 => 'sábado',
+    private const DOW_KEYS = [
+        0 => 'sun',
+        1 => 'mon',
+        2 => 'tue',
+        3 => 'wed',
+        4 => 'thu',
+        5 => 'fri',
+        6 => 'sat',
     ];
 
     public function toArray(Request $request): array
     {
         return $this->collection->map(function ($location) {
-            $windows = $location->fields->map(function ($field) {
-                return $field->leaguesFields->map(function ($leagueField) use ($field) {
-                    $byDay = $leagueField->windows
-                        ->groupBy('day_of_week')
-                        ->map(function ($items, $dow) {
-                            $day = self::DOW_LABELS[$dow] ?? (string) $dow;
-                            return $items->map(function ($w) use ($day) {
-                                return [
-                                    'day' => $day,
-                                    'start' => sprintf('%02d:%02d', intdiv($w->start_minute,60), $w->start_minute%60),
-                                    'end' => sprintf('%02d:%02d', intdiv($w->end_minute,60), $w->end_minute%60),
-                                ];
-                            })->values()->toArray();
-                        })
-                        ->values() // remover claves numéricas para mantener arreglo simple
-                        ->toArray();
-                    return [
-                        'id' => $field->id,
-                        'name' => $field->name,
-                        'type' => $field->type,
-                        'windows' => $byDay,
-                    ];
-                });
-            })->flatten(1)->toArray();
+            $fields = $location->fields->map(function ($field) {
+                // Tomamos las ventanas del primer league_field (asumido de la liga actual)
+                $leagueField = $field->leaguesFields->first();
+                $windows = [];
+                if ($leagueField) {
+                    foreach ($leagueField->windows as $w) {
+                        $key = self::DOW_KEYS[$w->day_of_week] ?? null;
+                        if (is_null($key)) { continue; }
+                        $windows[$key] = $windows[$key] ?? [];
+                        $windows[$key][] = [
+                            'enabled' => (bool) $w->enabled,
+                            'start' => sprintf('%02d:%02d', intdiv($w->start_minute,60), $w->start_minute%60),
+                            'end' => sprintf('%02d:%02d', intdiv($w->end_minute,60), $w->end_minute%60),
+                        ];
+                    }
+                }
+                return [
+                    'id' => $field->id,
+                    'name' => $field->name,
+                    'windows' => (object) $windows,
+                ];
+            })->values()->toArray();
             return [
                 'id' => $location->id,
                 'name' => $location->name,
                 'address' => $location->address,
-                'windows' => $windows,
+                'fields' => $fields,
                 'fields_count' => $location->fields->count(),
                 'position' => $location->position,
                 'tags' => $location->tags->pluck('name'),

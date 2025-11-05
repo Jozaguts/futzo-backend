567ccb8faf3b5e30fbd5b407f1b9c0f389784de7
Jose Terrazas <jozaguts@gmail.com>
2025-11-05 12:27:24 -0600

    refactor(tournament event): should be broadcast removed

--- app/Events/TournamentCreatedEvent.php
+++ app/Events/TournamentCreatedEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TournamentCreatedEvent implements ShouldBroadcast
+class TournamentCreatedEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 

9dff36782c0141bb5f42399fce55a54c6fb57b26
Jose Terrazas <jozaguts@gmail.com>
2025-11-22 15:12:33 -0600

    saving

--- routes/auth/routes.php
+++ routes/auth/routes.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticateController;
-use App\Models\User;
 use GuzzleHttp\Exception\ClientException;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
